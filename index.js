bf71c149287d4b2bf1ba5b19a849009c482d40e6
Robert <robethuku11@gmail.com>
2022-12-04 19:30:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code here!
+
+let wholePage = document.getElementsByTagName("body");
+
+
+
+function removeHeading() {
+  let initialHeader = document.getElementById('main');
+
+  return document.body.removeChild(initialHeader)
+
+  
+  
+
+
+
+}
+
+removeHeading();
+
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Robert Thuku is the champion";
